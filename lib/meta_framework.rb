19ef128b421efd1f0fa0291b1659fc12c5853661
Yuichi Tateno <hotchpotch@gmail.com>
2008-08-05 12:57:40 +0900

    iroiro

--- lib/meta_framework.rb
+++ lib/meta_framework.rb
@@ -1,16 +1,43 @@
 
 require 'pathname'
 $LOAD_PATH.unshift Pathname.new(__FILE__).parent
+$LOAD_PATH.uniq!
 require 'meta_framework/vim'
 
 class MetaFramework
   include VIM
 
-  def sid(sid)
+  def self.sid=(sid)
     @@sid = sid
   end
-  def sid
+  def self.sid
     @@sid
+  end
+
+  @@buffers = {}
+  def self.registry_buffer(buffer = nil)
+    buffer ||= Buffer.current
+    @@buffers[buffer.number] ||= buffer
+  end
+
+  @@frameworks = {}
+  def self.search(path)
+    path = path.parent.realpath
+    @@frameworks.each do |key, framework|
+      return framework if path.to_s.index(key) == 0
+    end
+
+    count = 0
+    while path.to_s != '/' || count < 20
+      return new path if root? path
+      count += 1
+      path = path.parent
+    end
+    nil
+  end
+
+  def self.root?(path)
+    path.join('meta_framework.yml').exist?
   end
 
   def initialize(root)
@@ -21,11 +48,16 @@ class MetaFramework
   class Buffer
     def initialize(buffer)
       @buffer = buffer
+      @framework = MetaFramework.search(path)
     end
     attr_reader :buffer
 
+    def number
+      buffer.number
+    end
+
     def path 
-      Pathname.new VIM::Function.expand("##{buffer.number}:p")
+      Pathname.new VIM::Function.expand("##{number}:p")
     end
 
     def self.current
--- meta_framework.vim
+++ meta_framework.vim
@@ -4,21 +4,23 @@
 "  #p VIM::Method.expand('#1:P')
 "EOF
 
-"function! s:sub(str,pat,rep)
-"  return substitute(a:str,'\v\C'.a:pat,a:rep,'')
-"endfunction
-"
-"map <SID>xx <SID>xx
-"let g:MetaFrameworkSID = s:sub(maparg("<SID>xx"),'xx$','')
-"unmap <SID>xx
+function! s:sub(str,pat,rep)
+  return substitute(a:str,'\v\C'.a:pat,a:rep,'')
+endfunction
 
-let s:meta_framework_file = expand('<sfile>:p:h') . '/lib/meta_framework.rb'
+map <SID>xx <SID>xx
+let g:MetaFrameworkSID = s:sub(maparg("<SID>xx"),'xx$','')
+unmap <SID>xx
+
+let s:meta_framework_file = fnamemodify(resolve(expand('<sfile>:p')), ':p:h') . '/lib/meta_framework.rb'
 execute 'rubyfile ' . s:meta_framework_file
 
+autocmd BufEnter * ruby MetaFramework.registry_buffer
 "augroup metaFramework
 "  autocmd!
-"  autocmd User BufEnterMetaFramework ruby MetaFramework::Buffer.new
-"  autocmd BufEnter * silent doau User
+"  autocmd User BufEnterMetaFramework call echo 'hello'
+"  autocmd User BufEnterMetaFramework ruby MetaFramework.registry_buffer
+"  autocmd BufEnter * doau User
 "  
 "  " autocmd User BufEnterMetaFramework call s:RefreshBuffer()
 "augroup END
